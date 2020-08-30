41f7d9a504ad50545cb2cc41d0b10abbd3c1bb39
Adrian Apodaca <aapodaca@gmail.com>
2020-08-30 16:04:22 -0700

    fix app.js

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
-const mongoose = require('mongoose');
 const db = require("./config/keys").mongoURI;
+const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+
 const users = require("./routes/api/users");
 const jobs = require("./routes/api/jobs");
 
-app.use("/api/users", users);
-app.use("/api/jobs", jobs);
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-const User = require("./models/User");
-
+// const User = require("./models/User");
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to mongoDB"))
   .catch((err) => console.log(err));
 
-
 app.get("/", (req, res) => res.send("I am shonails"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
+app.use("/api/jobs", jobs);
+
+
+
 
 
 const port = process.env.PORT || 5000;
