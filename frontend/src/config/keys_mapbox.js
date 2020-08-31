2e6134c53a750af08048aa9f89e6d5511d1c9b27
Adrian Apodaca <62411584+mufasubhai@users.noreply.github.com>
2020-08-31 15:23:53 -0700

    Delete keys_mapbox.js

--- frontend/src/config/keys_mapbox.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  public_key: "pk.eyJ1Ijoia3NoaWZsZXR0ODgiLCJhIjoiY2tlaXYydGx5MDBvZDMwcDN0cTk5OTh6NSJ9.jO353Xkq7wIYUY1OAoKPYQ",
-  secret_key: "sk.eyJ1Ijoia3NoaWZsZXR0ODgiLCJhIjoiY2tlaXd0bXd6MWthbDJ0cHFzems0YnBsZCJ9.z-jlkS3doYFKFXoQ7sf-zw",
-  googleMapsKey: "AIzaSyBpU5an4A-Q4Kdr9vIi4vVZfJlHIB28aoU", 
-};
-
