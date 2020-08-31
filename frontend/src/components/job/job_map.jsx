9d5a8e7d665ef1bddc6d8527641c0a26d1a0b1dd
kshiflett88 <kodishiflett@gmail.com>
2020-08-31 15:00:23 -0700

    add gitignore to keys

--- frontend/src/components/job/job_map.jsx
+++ frontend/src/components/job/job_map.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
 import { withRouter, link } from 'react-router-dom';
 import Typical from 'react-typical';
+import keys from '../../config/keys_mapbox'
+
+
+const mapOptions = {
+  center: {
+    lat: 37.773972,
+    lng: -122.431297,
+  }, // San Francisco coords
+  zoom: 13,
+};
+
 
 class JobMap extends React.Component {
-    constructor(props) {
-        super(props)
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <div id="map-container"> This is the map container</div>
-        )
-    }
+  componentDidMount() {
+    // set the map to show SF
+    const mapOptions = {
+      center: { lat: 37.7758, lng: -122.435 }, // this is SF
+      zoom: 13,
+    };
+
+    // wrap this.mapNode in a Google Map
+    // this.map = new google.maps.Map(this.mapNode, mapOptions);
+  }
+
+  render() {
+    return <div id="map-container" ref={ map => this.mapNode = map }> This is the map container</div>;
+  }
 }
+
+export default JobMap;
---
+++ frontend/src/config/keys_dev_front.js
@@ -0,0 +1,4 @@
+module.exports = {
+  googleMapsKey: "AIzaSyBpU5an4A-Q4Kdr9vIi4vVZfJlHIB28aoU",
+  
+};
