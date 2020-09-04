d8d11753e4b3cc50be9c3095842bbf3d2d43f53e
Shanelle Valencia <sgev808@hawaii.edu>
2020-09-04 14:44:38 -0700

    remove a file from src

---
+++ frontend/src/config/keys_dev_front.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleMapsKey: "AIzaSyBpU5an4A-Q4Kdr9vIi4vVZfJlHIB28aoU",
+  mapBoxPublicKey:
+    "pk.eyJ1Ijoia3NoaWZsZXR0ODgiLCJhIjoiY2tlaXYydGx5MDBvZDMwcDN0cTk5OTh6NSJ9.jO353Xkq7wIYUY1OAoKPYQ",
+  mapBoxSecretKey:
+    "sk.eyJ1Ijoia3NoaWZsZXR0ODgiLCJhIjoiY2tlaXd0bXd6MWthbDJ0cHFzems0YnBsZCJ9.z-jlkS3doYFKFXoQ7sf-zw",
+};
