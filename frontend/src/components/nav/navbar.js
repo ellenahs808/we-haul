053cdae73897a0cea011f71a7da7791817b57c4c
Adrian Apodaca <aapodaca@gmail.com>
2020-09-23 17:06:28 -0700

    add fetchcurrentuser thunk action

--- frontend/src/components/nav/navbar.js
+++ frontend/src/components/nav/navbar.js
@@ -1,42 +1,94 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import "../../styles/navbar.scss";
-// import logoImg from '../images/logo.png';
 import trucklogo from '../images/trucklogo.png'
-
+import StarRatings from 'react-star-ratings'
 
 class NavBar extends React.Component {
     constructor(props) {
         super(props);
         this.logoutUser = this.logoutUser.bind(this);
-        // this.getLinks = this.getLinks.bind(this);
+        this.user = {};
     }
 
     logoutUser(e) {
         e.preventDefault();
         this.props.logout();
     }
+    componentDidMount() {
+      debugger
+       this.user = this.props.fetchCurrentUser(this.props.currentUser.id);
 
-    componentWillUpdate() {
+    }
+    componentDidUpdate() {
         window.location.reload(false);
     }
+    // componentWillUpdate() {
+    //     window.location.reload(false);
+    // }
 
 
     getLinks() {
-        if (this.props.loggedIn) {
-            if (this.props.session.user.userType === 'hauler'){
+      if (this.props.loggedIn) {
+        const starRate = (this.props.currentUser.rating / this.props.currentUser.numberOfRatings);
+        const userName = this.props.currentUser.firstName;
+        if ((this.props.session.user.userType === 'hauler') && (this.props.currentUser.numberOfRatings > 0)){
             return (
-                    <div>
-                        <div className='nav-jobs'><Link to='/jobs' className='jobs-link'>Jobs</Link></div>
-                        <div className='nav-logout'><button onClick={this.logoutUser} className='nav-logout-button'>Logout</button></div>
-                    </div>
-            )} else{
+              <div>
+                <div className="nav-rating">
+                  
+                  <p className="nav-welcome">Welcome, {userName} your rating is:</p>
+                  <div className='nav-stars'>
+                    <StarRatings
+                        rating={starRate}
+                        ingnoreInlineStyles={false}
+                        starDimension='22px'
+                        starRatedColor="purple"
+                        numberOfStars={5}
+                        name="rating"
+                        starSpacing='1px'
+                    />
+                  </div>
+                </div>
+                <div className="nav-jobs">
+                  <Link to="/jobs" className="jobs-link">
+                    Jobs
+                  </Link>
+                </div>
+                <div className="nav-logout">
+                  <button
+                    onClick={this.logoutUser}
+                    className="nav-logout-button"
+                  >
+                    Logout
+                  </button>
+                </div>
+              </div>
+            );} else{
+          
                 return (
-                    <div>
-                        <div className='nav-jobs'><Link to='/userjob' className='jobs-link'>Requests</Link></div>
-                        <div className='nav-logout'><button onClick={this.logoutUser} className='nav-logout-button'>Logout</button></div>
-                    </div>     
-            )}
+                  <div>
+                    <div className="nav-rating">
+                      <p className="nav-welcome">
+                        Welcome, {userName}
+                      </p>
+
+                    </div>
+                    <div className="nav-jobs">
+                      <Link to="/userjob" className="jobs-link">
+                        Requests
+                      </Link>
+                    </div>
+                    <div className="nav-logout">
+                      <button
+                        onClick={this.logoutUser}
+                        className="nav-logout-button"
+                      >
+                        Logout
+                      </button>
+                    </div>
+                  </div>
+                );}
         } else {
             return (
                     <div>
@@ -49,12 +101,11 @@ class NavBar extends React.Component {
 
     render() {
         return (
-          <div className='navbar'>
+            <div className='navbar'>
             <div onClick={e => e.stopPropagation()} className='nav-session' >
                 <div className='nav-session'>
                     <div><Link to='/'><img src={trucklogo} className='logo-nav' alt="truck-logo"></img></Link></div>
-                    <div>
-                    </div>
+                
                     {this.getLinks()}
                 </div>
             </div>
--- frontend/src/components/nav/navbar_container.js
+++ frontend/src/components/nav/navbar_container.js
@@ -1,19 +1,22 @@
 import { connect } from 'react-redux';
 import { logout } from '../../actions/session_actions';
 import { openModal} from '../../actions/modal_actions';
+import  { fetchCurrentUser} from '../../actions/session_actions'
 import NavBar from './navbar';
 
 
 
 const mapStateToProps = state => ({
     loggedIn: state.session.isAuthenticated,
-    session: state.session
+    session: state.session,
+    currentUser: state.session.
 });
 
 
 const mapDispatchToProps = dispatch => ({
     logout: () => dispatch(logout()),
     openModal: type => dispatch(openModal(type)),
+    fetchCurrentUser: userId => dispatch(fetchCurrentUser(userId))
 })
 
 
